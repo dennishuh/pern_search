ea1110aec8461919a90c839fa7eed51eef4459a9
Dennis Huh <denjhuh@gmail.com>
2022-07-11 22:32:01 -0500

    Ready for deployment to heroku

--- server/db/connect.js
+++ server/db/connect.js
@@ -1,13 +1,19 @@
 const Pool = require('pg').Pool;
 
-console.log('process.env.DB_USER:', process.env.DB_USER);
-
-const pool = new Pool({
+const devConfig = {
 	user: process.env.DB_USER,
 	password: process.env.DB_PASSWORD,
 	host: 'localhost',
 	port: '5432',
 	database: 'partydb',
-});
+};
+
+const prodConfig = {
+	connectionString: process.env.DATABASE_URL,
+};
+
+const pool = new Pool(
+	process.env.NODE_ENV === 'production' ? prodConfig : devConfig
+);
 
 module.exports = pool;
--- server/index.js
+++ server/index.js
@@ -3,36 +3,25 @@ require('dotenv').config();
 
 const express = require('express');
 const cors = require('cors');
+const path = require('path');
 const userRouter = require('./routes/userRoutes');
 
-console.log('process.env.PORT', process.env.PORT);
 const PORT = process.env.PORT || 5000;
 const app = express();
 
 app.use(cors());
 app.use(express.json());
 
+if (process.env.NODE_ENV === 'production') {
+	// serve static content
+	app.use(express.static(path.join(__dirname, 'client/build')));
+}
+
 app.use('/api/v1/users', userRouter);
 
-// app.use('/api/v1/jobs', jobsRouter);
-
-// app.get('/api/v1/users', async (req, res) => {
-// 	try {
-// 		const { query } = req.query;
-// 		console.log('query', query);
-
-// 		const users = await pool.query(
-// 			'SELECT * FROM users WHERE first_name || " " || last_name ILIKE $1',
-// 			[`%${query}%`]
-// 		);
-
-// 		console.log('users.row', users.row[0]);
-
-// 		res.send({ test: 'dennis' });
-// 	} catch (error) {
-// 		console.error(error.message);
-// 	}
-// });
+app.get('*', (req, res) => {
+	res.sendFile(path.join(__dirname, 'client/build/index.html'));
+});
 
 app.listen(PORT, () => {
 	console.log(`Server is starting on port ${PORT}`);
